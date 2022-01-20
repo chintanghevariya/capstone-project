3c7eb42efa19d7fe7335638762559f4c4687f5d0
Rutik Patel <rutikpatel3660@gmail.com>
2022-01-20 16:00:42 -0500

    update handleSubmit()

--- api/app.js
+++ api/app.js
@@ -21,11 +21,12 @@ app.use(cors())
 
 // Start app on environment port
 app.listen(config.PORT, async function () {
-    console.log(`Listening on port ${config.port}`);
-    console.log(`Connecting to mongo`);
+    console.log(`Listening on port ${config.PORT}`);
+    console.log(`Connecting to mongo...`);
     const { MONGO_PORT, MONGO_DB_NAME, MONGO_HOST } = config
     try {
-        await mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`);
+        // await mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`);
+        await mongoose.connect(`mongodb+srv://rutikpatel:Rutik123@com3123.4rasi.mongodb.net/test`);
         console.log("Connected to mongo");
     } catch (e) {
         console.error(e);
