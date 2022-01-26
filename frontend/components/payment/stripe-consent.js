4cbbfe068e22498c9dd30d2195e7664a67ba5549
Aary Trivedi <aarytrivedi@gmail.com>
2022-01-26 12:42:12 -0500

    Adds text

--- frontend/components/payment/stripe-consent.js
+++ frontend/components/payment/stripe-consent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'native-base'
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { createCustomer } from '../../api/stripe';
 
 export default function StripeConsent() {
@@ -19,6 +19,19 @@ export default function StripeConsent() {
 
     return (
         <View style={styles.container}>
+            <Text>
+                This app stores data card information securely on stripe.
+            </Text>
+            <Text>
+                By clicking continue you grant us right to create register you as a customer of our service on stripe.
+            </Text>
+            <Text>
+                You also consent to let us store your card information on stripe securely.
+            </Text>
+            <Text>
+                Note: you must grant access to let us setup your credit card or debit card for payment.
+            </Text>
+            <Text>   </Text>
             <Button
                 width={"sm"}
                 backgroundColor={"#21A656"}
