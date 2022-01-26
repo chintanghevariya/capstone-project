576c0d9b8fa49e272de47fc632f43341dfa8cfa8
Aary Trivedi <aarytrivedi@gmail.com>
2022-01-26 12:53:44 -0500

    Adds stripe provider

--- frontend/App.js
+++ frontend/App.js
@@ -5,7 +5,7 @@ import {
 } from "native-base";
 import Login from "./components/login";
 import StripeConsent from "./components/payment/stripe-consent";
-
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 // Define the config
 const config = {
@@ -18,11 +18,12 @@ export const theme = extendTheme({ config });
 
 export default function App() {
   return (
-    <>
-    <NativeBaseProvider>
-      <StripeConsent />
-    </NativeBaseProvider>
-    </>
+    <StripeProvider
+      publishableKey="pk_test_51JipKvJSijMdla1x0pJ2EjkQpvgsfESI7zbqPwExNQ9nHlGTy2XUXk0kZc2Tq9J6XiYmzGm5umC3U9gP2zetDD6K00TOImtlbb">
+      <NativeBaseProvider>
+        <StripeConsent />
+      </NativeBaseProvider>
+    </StripeProvider>
   );
 }
 
