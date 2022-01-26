bc4923b5e24caea89ae8a800dba64812ce7f2acf
Aary Trivedi <aarytrivedi@gmail.com>
2022-01-26 13:20:00 -0500

    Implements setting up payment method

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,7 @@ import {
 import Login from "./components/login";
 import StripeConsent from "./components/payment/stripe-consent";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import SetupCustomerCard from "./components/payment/setup-customer-card";
 
 // Define the config
 const config = {
@@ -21,7 +22,7 @@ export default function App() {
     <StripeProvider
       publishableKey="pk_test_51JipKvJSijMdla1x0pJ2EjkQpvgsfESI7zbqPwExNQ9nHlGTy2XUXk0kZc2Tq9J6XiYmzGm5umC3U9gP2zetDD6K00TOImtlbb">
       <NativeBaseProvider>
-        <StripeConsent />
+        <SetupCustomerCard />
       </NativeBaseProvider>
     </StripeProvider>
   );
