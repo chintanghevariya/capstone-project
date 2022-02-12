8e17fd484923ef201ee83fadb0cdd558cc708f0d
Aary Trivedi <aarytrivedi@gmail.com>
2022-02-12 13:17:53 -0500

    Adds number of rides property

--- api/models/user.js
+++ api/models/user.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
     driverDetailsValid: {
         type: Boolean,
         default: false
+    },
+    numberOfRides: {
+        type: Number,
+        default: 0
     }
 });
 
